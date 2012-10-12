3bf04b833851059b47f5757bdcd1f757298536b9
Sacha Greif <sacha357@gmail.com>
2012-10-12 10:27:52 +0900

    initial commit

---
+++ public/javascripts/main-ck.js
@@ -0,0 +1,1 @@
+$(function(){$(".card").addClass("animate");$(".language-toggle .en").click(function(){$("body").removeClass("jp").addClass("en")});$(".language-toggle .jp").click(function(){$("body").removeClass("en").addClass("jp")})});
---
+++ readme.md
@@ -0,0 +1,1 @@
+# HNKansai.org static site #
